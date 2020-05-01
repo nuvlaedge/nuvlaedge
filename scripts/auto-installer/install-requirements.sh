08c11e3c528a5a3620d0c0c130dcb47007ba7b1b
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2020-05-01 14:28:10 +0200

    increase feedback time

--- scripts/auto-installer/install-requirements.sh
+++ scripts/auto-installer/install-requirements.sh
@@ -3,6 +3,8 @@
 apt-get update
 apt-get install -y inotify-tools ntfs-3g usbmount wget jq
 
+# install docker and docker-compose 1.23+ , swarm mode
+
 # might defer depending on the systemd-udev installed:
 sed -i 's/PrivateMounts=yes/PrivateMounts=no/' /lib/systemd/system/systemd-udevd.service
 

0b5f614c1b93fa29ffcaf8cdc1f1b43651c3ad12
Amit Bhanja <amit.bhanja@sixsq.com>
2023-09-28 13:51:10 +0200

    Adding asyncio support to peripherals

--- nuvlaedge/peripherals/gpu/__main__.py
+++ nuvlaedge/peripherals/gpu/__main__.py
@@ -1,9 +1,5 @@
 from nuvlaedge.peripherals.gpu import main
 import asyncio
 
-<<<<<<< HEAD
-def entry():
-=======
 if __name__ == "__main__":
->>>>>>> 9b89ecf (Adding asyncio support to peripherals)
     asyncio.run(main())

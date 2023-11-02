6f1babe3fd34ef7ccc79e190ea42420a2df071e4
John White <3775436+giovannibianco@users.noreply.github.com>
2023-11-02 19:45:42 +0100

    fix(modbus): Fix the modbus detection from kubernetes NuvlaEdge (#116)
    ---------
    
    Co-authored-by: schaubl <schaubl@users.noreply.github.com>

--- nuvlaedge/peripherals/modbus/__init__.py
+++ nuvlaedge/peripherals/modbus/__init__.py
@@ -18,7 +18,11 @@ import socket
 import struct
 import os
 import logging
+import fileinput
+import sys
 import asyncio
+import ipaddress
+import subprocess
 
 from nuvlaedge.peripherals.peripheral import Peripheral
 from nuvlaedge.common.nuvlaedge_config import parse_arguments_and_initialize_logging
@@ -43,6 +47,16 @@ def get_default_gateway_ip():
 
             return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
+def nmap_replace_port(file, search_exp, replace_exp):
+    '''
+    Use this to change the port number in the nmap modbus script
+    We keep this as a reminder that this may need to be done in the future.
+    '''
+
+    for line in fileinput.input(file, inplace=1):
+        if search_exp in line:
+            line = line.replace(search_exp,replace_exp)
+        sys.stdout.write(line)
 
 def scan_open_ports(host, modbus_nse="modbus-discover.nse", xml_file="/tmp/nmap_scan.xml"):
     """ Uses nmap to scan all the open ports in the NuvlaEdge.
@@ -55,17 +69,27 @@ def scan_open_ports(host, modbus_nse="modbus-discover.nse", xml_file="/tmp/nmap_
     :returns XML filename where to write the nmap output
     """
 
-    logger.info(f"Scanning ModBus ports in address {host}")
+    ports_range = "-p-"
+    # alternate_modbus_port = ""
+    # if alternate_modbus_port:
+      #   ports_range = "-p " + alternate_modbus_port
+        # nmap_replace_port("/usr/share/nmap/scripts/" + \
+          #   modbus_nse, "port_or_service(502,", "port_or_service(" + alternate_modbus_port + ",")
 
-    command = "nmap --script {} --script-args='modbus-discover.aggressive=true' -p- {} -T4 -oX {} > /dev/null" \
+    logger.info("Scanning open ports...")
+
+    command = \
+        "nmap --script {} --script-args='modbus-discover.aggressive=true' {} {} -T4 -oX {} > /dev/null"\
         .format(modbus_nse,
+                ports_range,
                 host,
                 xml_file)
+
+    logger.debug(f'Generated command:\n{command}')
 
     os.system(command)
 
     return xml_file
-
 
 def manage_modbus_peripherals(ip_address):
     """ Takes care of posting or deleting the respective
@@ -77,6 +101,9 @@ def manage_modbus_peripherals(ip_address):
     #    modbus.{port}.{interface}.{identifier}
 
     # Ask the NB agent for all modbus peripherals matching this pattern
+    # probably the kubernetes stuff should go here?
+    # why not?
+
     logger.info(f'Starting modbus scan on {ip_address}')
     xml_file = scan_open_ports(ip_address)
 
@@ -84,28 +111,40 @@ def manage_modbus_peripherals(ip_address):
     parser.parse()
 
     all_modbus_devices = parser.get_modbus_details()
-    discovered_devices: dict = {}
-    if ip_address not in all_modbus_devices:
-        logger.warning(f'No Modbus Info found for host : {ip_address}')
-        return discovered_devices
+    logger.debug(f"All modbus devices:\n {all_modbus_devices}")
 
-    for per in all_modbus_devices[ip_address]:
-        port = per.get("port", "nullport")
-        interface = per.get("interface", "nullinterface")
-        identifiers = per.get("identifiers").copy()
-        del per["identifiers"]
-        for ident in identifiers:
-            _id = ident['key']
-            per['classes'] = ident['classes']
-            per['vendor'] = ident['vendor']
-            per['name'] = ident['name']
-            identifier = "modbus.{}.{}.{}".format(port, interface, _id)
-            # Redefine the identifier
-            per['identifier'] = identifier
-            discovered_devices[identifier] = per.copy()
+    discovered_devices: dict = {}
+
+    for returned_ip_address in all_modbus_devices.keys():
+        for per in all_modbus_devices[returned_ip_address]:
+            port = per.get("port", "nullport")
+            interface = per.get("interface", "nullinterface")
+            identifiers = per.get("identifiers").copy()
+            del per["identifiers"]
+            for ident in identifiers:
+                _id = ident['key']
+                per['classes'] = ident['classes']
+                per['vendor'] = ident['vendor']
+                per['name'] = ident['name']
+                per['testhost'] = returned_ip_address
+                identifier = "host {}:{} interface {} id {}".format(returned_ip_address, port, interface, _id)
+                # Redefine the identifier
+                per['identifier'] = identifier
+                discovered_devices[identifier] = per.copy()
+
+    logger.debug(f"discovered devices:\n {discovered_devices}")
 
     return discovered_devices
 
+def determine_ip_addresses(list_of_ip_addresses):
+    """Determine the list of IP addresses to be scanned."""
+
+    if os.getenv('KUBERNETES_SERVICE_HOST') and os.getenv('MY_HOST_NODE_IP'):
+        list_of_ip_addresses = list_of_ip_addresses + ' ' + os.getenv('MY_HOST_NODE_IP')
+
+    logging.info(f'The list of IP addresses has been set to:\n{list_of_ip_addresses}')
+
+    return list_of_ip_addresses
 
 async def main():
     global logger
@@ -116,9 +155,15 @@ async def main():
 
     gateway_ip = get_default_gateway_ip()
 
+    logging.info(f"Gateway IP found {gateway_ip}")
+
     modbus_peripheral: Peripheral = Peripheral('modbus')
 
-    await modbus_peripheral.run(manage_modbus_peripherals, ip_address=gateway_ip)
+    # do the ip address thing here?
+
+    list_of_ip_addresses = determine_ip_addresses(gateway_ip)
+
+    await modbus_peripheral.run(manage_modbus_peripherals, ip_address=list_of_ip_addresses)
 
 
 def entry():

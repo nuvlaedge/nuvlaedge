ba0f26b2ccd8f0156b67aea60716009a3b97b666
Nacho <nacho@sixsq.com>
2024-03-25 16:50:11 +0100

    add explicit format name to requirements export parameter

--- local-build.sh
+++ local-build.sh
@@ -8,14 +8,14 @@ export IMAGE_TAG_NAME=$IMAGE_ORG/$IMAGE_REPO:${3:-$nuvlaedge_version}
 
 
 poetry build --no-interaction --format=wheel
-poetry export -f requirements.txt --output requirements.txt --without-hashes --without-urls
-poetry export -f requirements.txt --output requirements.agent.txt --without-hashes --without-urls --with agent
-poetry export -f requirements.txt --output requirements.system-manager.txt --without-hashes --without-urls --with system-manager
-poetry export -f requirements.txt --output requirements.network.txt --without-hashes --without-urls --with network
-poetry export -f requirements.txt --output requirements.modbus.txt --without-hashes --without-urls --with modbus
-poetry export -f requirements.txt --output requirements.gpu.txt --without-hashes --without-urls --with gpu
-poetry export -f requirements.txt --output requirements.job-engine.txt --without-hashes --without-urls --with job-engine
-poetry export -f requirements.txt --output requirements.bluetooth.txt --without-hashes --without-urls --with bluetooth
+poetry export --format requirements.txt --output requirements.txt --without-hashes --without-urls
+poetry export --format requirements.txt --output requirements.agent.txt --without-hashes --without-urls --with agent
+poetry export --format requirements.txt --output requirements.system-manager.txt --without-hashes --without-urls --with system-manager
+poetry export --format requirements.txt --output requirements.network.txt --without-hashes --without-urls --with network
+poetry export --format requirements.txt --output requirements.modbus.txt --without-hashes --without-urls --with modbus
+poetry export --format requirements.txt --output requirements.gpu.txt --without-hashes --without-urls --with gpu
+poetry export --format requirements.txt --output requirements.job-engine.txt --without-hashes --without-urls --with job-engine
+poetry export --format requirements.txt --output requirements.bluetooth.txt --without-hashes --without-urls --with bluetooth
 
 mv dist/nuvlaedge-${nuvlaedge_version}-py3-none-any.whl dist/nuvlaedge-latest-py3-none-any.whl
 

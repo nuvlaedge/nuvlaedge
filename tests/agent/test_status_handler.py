0e6c5f50e3d53b7018252eaf5d5607f3495b0152
Nacho <nacho@sixsq.com>
2024-04-10 14:36:55 +0200

    add test to status unittest

--- tests/agent/test_status_handler.py
+++ tests/agent/test_status_handler.py
@@ -45,6 +45,12 @@ class TestStatusHandler(TestCase):
         self.test_status_handler.process_status()
         self.assertEqual(self.test_status_handler._status, 'OPERATIONAL')
 
+        # Test with one module in STOPPED
+        self.test_module.module_status = 'FAILING'
+        self.test_status_handler.add_module(self.test_module)
+        self.test_status_handler.process_status()
+        self.assertEqual(self.test_status_handler._status, 'OPERATIONAL')
+
         # Test with one module in FAILING
         self.test_module.module_status = 'FAILING'
         self.test_status_handler.add_module(self.test_module)

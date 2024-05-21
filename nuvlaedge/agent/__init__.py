5b66df14c93e6629fdd6573aa793c867362cfc29
Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>
2024-05-21 21:08:30 +0200

    fix: Agent logging env configuration (#182)
    * fix: Agent logging env configuration
    
    * let logging directory be None as default in logging settings
    
    * Update build-main-devel.yml: update EnricoMi/publish-unit-test-result-action to v2
    
    ---------
    
    Co-authored-by: Lionel <schaubl@users.noreply.github.com>

--- nuvlaedge/agent/__init__.py
+++ nuvlaedge/agent/__init__.py
@@ -2,8 +2,17 @@ import logging
 import socket
 from threading import Event
 
+from nuvlaedge.common.nuvlaedge_logging import set_logging_configuration, LoggingSettings
+
+logging_settings = LoggingSettings()
+set_logging_configuration(
+    debug=logging_settings.nuvlaedge_debug,
+    log_level=logging.getLevelName(logging_settings.nuvlaedge_log_level),
+    log_path=logging_settings.nuvlaedge_logging_directory,
+    disable_file_logging=logging_settings.disable_file_logging
+)
+
 from nuvlaedge.agent.common.legacy_support import transform_legacy_config_if_needed
-from nuvlaedge.common.nuvlaedge_logging import set_logging_configuration
 from nuvlaedge.agent.settings import AgentSettings, get_agent_settings
 from nuvlaedge.agent.nuvla.client_wrapper import NuvlaClientWrapper
 
@@ -12,10 +21,10 @@ def main():
     # We need to configure logging before importing any nuvlaedge module with loggers
     # so there is no need to reconfigure them after the import
     agent_settings = get_agent_settings()
-    set_logging_configuration(debug=agent_settings.nuvlaedge_debug,
-                              log_level=logging.getLevelName(agent_settings.nuvlaedge_log_level),
-                              log_path=agent_settings.nuvlaedge_logging_directory,
-                              disable_file_logging=agent_settings.disable_file_logging)
+    # set_logging_configuration(debug=agent_settings.nuvlaedge_debug,
+    #                           log_level=logging.getLevelName(agent_settings.nuvlaedge_log_level),
+    #                           log_path=agent_settings.nuvlaedge_logging_directory,
+    #                           disable_file_logging=agent_settings.disable_file_logging)
     agent_event: Event = Event()
 
     from nuvlaedge.common.constants import CTE

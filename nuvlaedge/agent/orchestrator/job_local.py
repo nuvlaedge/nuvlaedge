b2759933b8ede0b10cb60f253837c7c0348d9d1b
Lionel <schaubl@users.noreply.github.com>
2024-07-02 17:34:15 +0200

    feat(agent): job execution in agent (#187)
    * feat(agent): job execution in agent
    
    * deps: update nuvla-job-engine to minimum 4.1.1
    
    * add protocol to interface the job launcher for job-engine and JobLocal (#188)
    
    ---------
    
    Co-authored-by: Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>

---
+++ nuvlaedge/agent/orchestrator/job_local.py
@@ -0,0 +1,28 @@
+
+import logging
+
+from nuvla.job_engine.job.executor.executor import Executor, LocalOneJobQueue
+from nuvla.job_engine.job.job import Job
+
+from nuvlaedge.common.constant_files import FILE_NAMES
+from nuvlaedge.common.nuvlaedge_logging import get_nuvlaedge_logger
+
+
+logger: logging.Logger = get_nuvlaedge_logger(__name__)
+
+
+class JobLocal:
+    """
+    Execute jobs directly in the agent
+    """
+
+    def __init__(self, api):
+        super().__init__()
+        self.api = api
+
+    def is_nuvla_job_running(self, job_id, job_execution_id):
+        return False
+
+    def launch_job(self, job_id, *args, **kwargs):
+        _job = Job(self.api, LocalOneJobQueue(job_id), FILE_NAMES.root_fs)
+        Executor.process_job(_job)
---
+++ tests/agent/orchestrator/test_job_local.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import mock
+import unittest
+
+from mock import MagicMock
+
+from nuvlaedge.agent.orchestrator.job_local import JobLocal
+
+
+class JobLocalTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        api = MagicMock()
+        self.obj = JobLocal(api)
+
+    def test_init(self):
+        self.assertIsInstance(self.obj, JobLocal)
+
+    def test_is_nuvla_job_running(self):
+        job_id = 'fake-id-1'
+        job_exec_id = 'fake-exec-id-1'
+        self.assertFalse(self.obj.is_nuvla_job_running(job_id, job_exec_id))
+
+    @mock.patch('nuvla.job_engine.job.executor.executor.Executor.process_job')
+    def test_launch_job(self, mock_process_job):
+        job_id = 'fake-id-2'
+        job_exec_id = 'fake-exec-id-2'
+        nuvla_endpoint = 'https://fake-nuvla.io'
+        docker_image = 'sixsq/nuvlaedge:latest'
+
+        ret = self.obj.launch_job(
+            job_id, job_exec_id,
+            nuvla_endpoint, nuvla_endpoint_insecure=False,
+            api_key='credential/abc-def', api_secret='123-abc-456',
+            docker_image=docker_image)
+
+        self.assertIsNone(ret)
+        mock_process_job.assert_called_once()

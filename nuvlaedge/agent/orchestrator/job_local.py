f00b01e6f12a3fb62ebd58f4350c9f9497af7f8c
schaubl <schaubl@users.noreply.github.com>
2024-10-09 16:26:03 +0200

    wip: add timeout for job in agent and handle zombie jobs

--- nuvlaedge/agent/orchestrator/job_local.py
+++ nuvlaedge/agent/orchestrator/job_local.py
@@ -1,5 +1,7 @@
-
+import datetime
 import logging
+import os
+import sys
 
 from collections import deque
 from threading import Thread, Semaphore
@@ -12,6 +14,13 @@ from nuvlaedge.common.nuvlaedge_logging import get_nuvlaedge_logger
 
 
 logger: logging.Logger = get_nuvlaedge_logger(__name__)
+
+job_timeout_default = 3600
+job_timeout_str = os.getenv('JOB_DOCKER_TIMEOUT') or job_timeout_default
+try:
+    job_timeout = int(job_timeout_str)
+except ValueError:
+    job_timeout = job_timeout_default
 
 
 class TinyQueue:
@@ -42,6 +51,7 @@ class JobLocal:
         super().__init__()
         self.api = api
         self.running_job = ''
+        self.running_job_since = None
         self.previous_jobs = deque(maxlen=20)
         self.job_queue = TinyQueue()
         self.job_executor_thread = None
@@ -63,13 +73,25 @@ class JobLocal:
         while True:
             job_id = self.job_queue.get()
             self.running_job = job_id
+            self.running_job_since = datetime.datetime.now()
             self.log_state()
             _job = Job(self.api, LocalOneJobQueue(job_id), FILE_NAMES.root_fs)
+
             if _job.get('state') == JOB_RUNNING:
-                logger.info(f'Job {job_id} already in running state. Do nothing.')
+                logger.info(f'Job {job_id} already in running state. Seems to be a zombie job. Set it to failed')
+                self.set_job_failed(self.running_job, 'Zombie job')
                 continue
-            logger.debug(f'Running job {job_id} locally')
+
+            # TODO: remove this condition
+            if _job.get('action') == 'infinite':
+                import time
+                while True:
+                    time.sleep(100000)
+
+            logger.info(f'Running job {job_id} locally')
             Executor.process_job(_job)
+
+            self.running_job_since = None
             self.previous_jobs.append(job_id)
             self.running_job = ''
 
@@ -77,6 +99,9 @@ class JobLocal:
         return self.running_job == job_id
 
     def launch_job(self, job_id, *args, **kwargs):
+        if self.running_job_since and (datetime.datetime.now() - self.running_job_since) > datetime.timedelta(seconds=job_timeout):
+            self.set_job_failed(self.running_job, 'Job timed out in NuvlaEdge')
+            sys.exit(11)
         self.log_state()
         if job_id == self.running_job:
             logger.debug(f'Job {job_id} currently running')
@@ -95,3 +120,7 @@ class JobLocal:
             Jobs in the queue: {list(self.job_queue.queue)} 
             Last executed jobs: {list(self.previous_jobs)} 
         ''')
+
+    def set_job_failed(self, job_id, message='Job failed'):
+        self.api.edit(job_id, {'state': JOB_FAILED,
+                               'status-message': message})

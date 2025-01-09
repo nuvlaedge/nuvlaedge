8ce2fa45226854bea7b3ac61ca762d3c2cec3418
Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>
2025-01-09 15:57:15 +0100

    build: fixed a bug on missing poetry plugin (#235)

--- generate-requirements.sh
+++ generate-requirements.sh
@@ -4,6 +4,8 @@ OUTPUT_DIR=${1:-.}
 
 mkdir -p $OUTPUT_DIR || true
 
+poetry self add poetry-plugin-export
+
 poetry export --format requirements.txt --output ${OUTPUT_DIR}/requirements.tests.txt --without-hashes --without-urls \
     --with tests \
     --with agent \
